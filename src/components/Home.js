350c94a5b4f03557bdcbfaffc728fdd82e7f8d15
Sean Joseph <sean_joseph@protonmail.com>
2022-09-20 20:13:11 -0700

    Finished home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 
 function Home(){
     return (
         <main class="home">
-            Home
+            <div className="hero">
+                <h1>Hi, I'm Sean Joseph</h1>
+                <p>A  Web Developer with a serious passion for creating functional and dynamic user experiences. Check out some of my work! </p>
+                <button>
+                    <Link to="/projects">Projects</Link>
+                </button>
+            </div>
         </main>
     )
 }
